d479bbd96a17518328ff59d5717810824bac702a
mtnewton <mtnewton3@gmail.com>
2021-02-19 21:22:01 -0500

    CraftingStationRange - initial creation

---
+++ CraftingStationRange/CraftingStationRangePlugin.cs
@@ -0,0 +1,93 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+
+namespace CraftingStationRange
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    [HarmonyPatch]
+    class CraftingStationRangePlugin : BaseUnityPlugin
+    {
+        const string GUID = "net.mtnewton.craftingstationrange";
+
+        const string NAME = "CraftingStationRange";
+
+        const string VERSION = "1.0.0";
+
+        private static ConfigFile config;
+
+        private static ManualLogSource logger;
+
+        private static List<KeyValuePair<string, float>> stationRanges = new List<KeyValuePair<string, float>>();
+
+        void Awake()
+        {
+            logger = Logger;
+            config = Config;
+
+            BindStationRange("$piece_workbench");
+            BindStationRange("$piece_forge");
+            BindStationRange("$piece_stonecutter");
+            BindStationRange("$piece_artisanstation");
+
+            Harmony harmony = new Harmony(GUID);
+            harmony.PatchAll();
+
+            Logger.LogInfo(NAME + " loaded.");
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(CraftingStation), "Start")]
+        public static void LogStations(CraftingStation __instance)
+        {
+            float stationRange = GetStationRange(__instance.m_name);
+
+            if (stationRange == 0)
+            {
+                Destroy(__instance.m_areaMarker);
+                __instance.m_areaMarker = null;
+                __instance.m_rangeBuild = float.MaxValue;
+                logger.LogInfo(__instance.m_name + " set to max range.");
+                return;
+            }
+
+            __instance.m_rangeBuild = stationRange;
+            __instance.m_areaMarker.GetComponent<CircleProjector>().m_radius = stationRange;
+            __instance.m_areaMarker.GetComponent<CircleProjector>().m_nrOfSegments = (int)Math.Ceiling(Math.Max(5f, 4f * stationRange));
+            logger.LogInfo(__instance.m_name + " set to " + stationRange + " range");
+
+        }
+
+        public static float GetStationRange(string stationName)
+        {
+            KeyValuePair<string, float> stationNameRangePair = stationRanges.Find(station => station.Key == stationName);
+
+            if (stationNameRangePair.Equals(default(KeyValuePair<string, float>)))
+            {
+                stationNameRangePair = BindStationRange(stationName);
+            }
+
+            return stationNameRangePair.Value;
+        }
+
+        public static KeyValuePair<string, float> BindStationRange(string stationName)
+        {
+            KeyValuePair<string, float> stationNameRangePair = new KeyValuePair<string, float>(
+                stationName,
+                Math.Abs(config.Bind(
+                    "CraftingStationRange", 
+                    stationName, 
+                    (float) 0.0, 
+                    "This crafting stations range."
+                ).Value)
+            );
+
+            stationRanges.Add(stationNameRangePair);
+
+            return stationNameRangePair;
+        }
+    }
+}
---
+++ CraftingStationRange/README.md
@@ -0,0 +1,31 @@
+# CraftingStationRange Plugin for Valheim
+Unlimited range for crafting stations. Or configure a custom range.
+
+As a reference, the game default for the stations is 20.
+
+0 = unlimited
+
+## Configuration
+```
+[CraftingStationRange]
+
+## Station range is set to this amount.
+# Setting type: Single
+# Default value: 0
+$piece_workbench = 0
+
+## Station range is set to this amount.
+# Setting type: Single
+# Default value: 0
+$piece_forge = 0
+
+## Station range is set to this amount.
+# Setting type: Single
+# Default value: 0
+$piece_stonecutter = 0
+
+## Station range is set to this amount.
+# Setting type: Single
+# Default value: 0
+$piece_artisanstation = 0
+```
