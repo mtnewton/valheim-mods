78f8783a6dcdb3becca6d691e008db5e2d0f108c
mtnewton <mtnewton3@gmail.com>
2021-02-23 18:06:11 -0500

    gravekeeper - option to keep grave, unequip items, new icon

--- Gravekeeper/GravekeeperPlugin.cs
+++ Gravekeeper/GravekeeperPlugin.cs
@@ -1,6 +1,10 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Linq;
 
 namespace Gravekeeper
 {
@@ -12,13 +16,27 @@ namespace Gravekeeper
 
         private const string NAME = "Gravekeeper";
 
-        private const string VERSION = "1.1.2";
+        private const string VERSION = "1.2.0";
 
         private static ManualLogSource logger;
+
+        private static bool keepGravestone;
+
+        private static bool keepItemsEquipped;
+
+        private static ItemDrop.ItemData stoneItemData = null;
 
         void Awake()
         {
             logger = Logger;
+
+            keepGravestone = Config.Bind(NAME, "KeepGravestone", false,
+                "Should the gravestone stay in the world? (you still keep your items) One stone is added to the graves inventory to keep it standing."
+            ).Value;
+
+            keepItemsEquipped = Config.Bind(NAME, "KeepItemsEquipped", true,
+                "Should items stay equipped when you respawn?"
+            ).Value;
 
             var harmony = new Harmony(GUID);
             harmony.PatchAll();
@@ -28,28 +46,68 @@ namespace Gravekeeper
 
         [HarmonyPrefix]
         [HarmonyPatch(typeof(Inventory), "MoveInventoryToGrave")]
-        static void SaveInventory(out InventoryTracker __state, Inventory __instance, Inventory original)
+        static bool MoveInventoryToGrave_PrefixPatch(ref Inventory __instance)
         {
-            __state = new InventoryTracker(__instance, original);
+            __instance.m_inventory.Clear();
+            if (keepGravestone)
+            {
+                ItemDrop.ItemData stone = GetStoneItemData().Clone();
+                if (stone != null)
+                {
+                    __instance.m_inventory.Add(stone);
+                    logger.LogInfo("1 stone added to gravestone so that it remains.");
+                }
+            }
+            __instance.Changed();
+            return false;
         }
 
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(Inventory), "MoveInventoryToGrave")]
-        static void RestoreInventory(InventoryTracker __state)
-        {
-            __state.PlayerInventory.MoveAll(__state.GraveInventory);
-            logger.LogInfo(__state.PlayerInventory.GetAllItems().Count + " items recovered.");
-        }
-    }
+        private static MethodInfo UnequipAllItems = AccessTools.Method(typeof(Humanoid), "UnequipAllItems");
 
-    class InventoryTracker
-    {
-        public InventoryTracker(Inventory graveInventory, Inventory playerInventory)
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(Player), "CreateTombStone")]
+        static IEnumerable<CodeInstruction> CreateTombstone_TranspilerPatch(IEnumerable<CodeInstruction> instructions)
         {
-            GraveInventory = graveInventory;
-            PlayerInventory = playerInventory;
+            if (!keepItemsEquipped)
+            {
+                logger.LogInfo("Equipment will be unequipped.");
+
+                return instructions;
+            }
+
+            List<CodeInstruction> il = instructions.ToList();
+           
+            for (int i = 0; i < il.Count; ++i)
+            { 
+                if (il[i].Calls(UnequipAllItems))
+                {
+                    il[i - 1].opcode = OpCodes.Nop;
+                    il[i].opcode = OpCodes.Nop;
+                }
+            }
+
+            logger.LogInfo("Equipment will remain equipped.");
+
+            return il.AsEnumerable();
         }
-        public Inventory GraveInventory { get; set; }
-        public Inventory PlayerInventory { get; set; }
+
+        private static ItemDrop.ItemData GetStoneItemData()
+        {
+            if (stoneItemData != null)
+            {
+                return stoneItemData;
+            }
+
+            List<ItemDrop> items = ObjectDB.m_instance.GetAllItems(ItemDrop.ItemData.ItemType.Material, "Stone");
+            foreach (ItemDrop item in items)
+            {
+                if (item.m_itemData.m_shared.m_name == "$item_stone")
+                {
+                    stoneItemData = item.m_itemData;
+                    break;
+                }
+            }
+            return stoneItemData;
+        }
     }
 }
--- Gravekeeper/README.md
+++ Gravekeeper/README.md
@@ -1,22 +1,46 @@
-# Gravekeeper Plugin for Valheim
-Restores your items from the grave.
-Keep your items when you die.
+# Gravekeeper Plugin for Valheim  
+Keep your items when you die.  
+Config to keep the gravestone from crumbling (1 stone added to its inventory).  
+Config to unequip items on death.  
 
-## Installation
+## Configuration  
+`<GameLoacation>/BepInEx/config/net.mtnewton.gravekeeper.cfg`
+```
+[Gravekeeper]
+
+## Should the gravestone stay in the world? (you still keep your items) One stone is added to the graves inventory to keep it standing.
+# Setting type: Boolean
+# Default value: false
+KeepGravestone = false
+
+## Should items stay equipped when you respawn?
+# Setting type: Boolean
+# Default value: true
+KeepItemsEquipped = true
+```
+
+## Installation  
 1. Download and install [BepInEx Valheim](https://valheim.thunderstore.io/package/denikson/BepInExPack_Valheim/)
 2. Download this mod and move the `Gravekeeper.dll` into `<GameLocation>\BepInEx\plugins`
+3. Launching the game will generate a config file at `<GameLocation>\BepInEx\config`
 
-## Changelog
-- v1.1.2
-  - updated readme, mod does not generate a config file
-- v1.1.1
-  - fix readme dll name and typos
-- v1.1.0
-  - readme changes - installation steps and changelog
-  - refactor code for simplification
-- v1.0.0
-  - initial mod creation
-  - gives your items back to you after you die
+## Changelog  
+- v1.2.0  
+  - new icon - no one told me that was the back of the gravestone XD  
+  - under the hood - now prevents items from moving to grave at all. used to MoveAll items from grave inventory back to players inventory  
+  - now has a config  
+    - KeepGravestone(false) - option to keep gravestone in world (1 stone added grave inventory)  
+    - KeepItemsEquipped(true) - option to unequip items when player dies  
+- v1.1.2  
+  - updated readme, mod does not generate a config file  
+- v1.1.1  
+  - fix readme dll name and typos  
+- v1.1.0  
+  - readme changes - installation steps and changelog  
+  - refactor code for simplification  
+- v1.0.0  
+  - initial mod creation  
+  - gives your items back to you after you die  
 
-## Source Code
+## Source Code  
 https://github.com/mtnewton/valheim-mods/tree/master/Gravekeeper
